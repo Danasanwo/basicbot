f6e08f4947ebcf185a6cb43e4e778abda1864d2e
Damilola Anasanwo <damilolaanasanwo@Chimas-MacBook-Pro.local>
2022-03-22 23:32:54 +0000

    6th

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const run = () => {
         base : 'USDT',
         allocation : 0.7,
         spread : 0.001,
-        tickInterval: 60000
+        tickInterval: 600000
     }
 
     const binanceClient = new ccxt.binance({
